366465744ff17a94695d673b8b7842c6ad3272ec
Alexander La Cruz <mvlctrading@gmail.com>
2022-12-05 18:58:57 -0500

    mvt_desafio

--- app_desafio/views.py
+++ app_desafio/views.py
@@ -1,14 +1,14 @@
 from ast import Return
 from django.http import HttpResponse
-from django.template import Template, context, loader
+from django.template import loader
 from app_desafio.models import Familiar
 
 # Create your views here.
 
 def listar_familiar(request):
-    queryset = Familiar.objects.all()
-    diccionario = {'app_desafio': queryset}
-    plantilla = loader.get_template('familiar_listado.html')
+    family = Familiar.objects.all()
+    diccionario = {'app_desafio': family}
+    plantilla = loader.get_template('listado_familia.html')
     documento_html = plantilla.render(diccionario)
 
     return HttpResponse(documento_html)
